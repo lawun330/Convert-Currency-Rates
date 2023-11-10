fd1db7e0ac3e971881f90e2ee08be425162496c1
La Wun Nannda <60000347+lawun330@users.noreply.github.com>
2023-11-10 17:43:21 +0630

    Create QuestionDialog.java
    A button-listener class to listen the user's click on available options.

---
+++ QuestionDialog.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
+
+public class QuestionDialog extends JDialog implements ActionListener {
+
+  // instance variable to store answer
+  public String answer;
+
+  // implementation of a method in ActionListener interface
+  public void actionPerformed(ActionEvent e) {
+    answer = e.getActionCommand(); // label of the button that generated event "e"
+    dispose(); // destroy all components
+  }
+}
